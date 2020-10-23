f38a1d15cc8684528585edcdaac965b199bfb809
Yury Semikhatsky <yurys@chromium.org>
2020-10-23 12:28:11 -0700

    chore: update license formatting to not look like javadoc (#38)

--- playwright/src/main/java/com/microsoft/playwright/impl/PageImpl.java
+++ playwright/src/main/java/com/microsoft/playwright/impl/PageImpl.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) Microsoft Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,8 +21,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.microsoft.playwright.*;
 
-import java.io.*;
-import java.nio.file.Watchable;
+import java.io.File;
 import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
--- playwright/src/main/java/com/microsoft/playwright/impl/RouteImpl.java
+++ playwright/src/main/java/com/microsoft/playwright/impl/RouteImpl.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) Microsoft Corporation.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
