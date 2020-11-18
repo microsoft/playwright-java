a6d5fdf06d9441569975200bfb862d4349bc3f43
Yury Semikhatsky <yurys@chromium.org>
2020-11-17 16:23:30 -0800

    Timeout while installing

--- playwright/src/main/java/com/microsoft/playwright/impl/PlaywrightImpl.java
+++ playwright/src/main/java/com/microsoft/playwright/impl/PlaywrightImpl.java
@@ -73,7 +73,7 @@ public class PlaywrightImpl extends ChannelOwner implements Playwright {
       ProcessBuilder pb = new ProcessBuilder(driver.toString(), "install");
       pb.redirectError(ProcessBuilder.Redirect.INHERIT);
       Process p = pb.start();
-      int result = p.waitFor();
+      boolean result = p.waitFor(30, TimeUnit.SECONDS);
       System.out.println("done playwright-cli install: " + result);
     }
     return driverTempDir.resolve("playwright-cli");
