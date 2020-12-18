af8ec69d9b475b50c57dc32c7fa940b4c4eac379
Yury Semikhatsky <yurys@chromium.org>
2020-12-18 11:55:44 -0800

    chore: add examples project

---
+++ examples/src/main/java/org/example/EvaluateInBrowserContext.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example;
+
+import com.microsoft.playwright.*;
+
+public class EvaluateInBrowserContext {
+  public static void main(String[] args) throws Exception {
+    Playwright playwright = Playwright.create();
+    BrowserType browserType = playwright.firefox();
+    Browser browser = browserType.launch();
+    BrowserContext context = browser.newContext();
+    Page page = context.newPage();
+    page.navigate("https://www.example.com/");
+    Object dimensions = page.evaluate("() => {\n" +
+        "  return {\n" +
+        "      width: document.documentElement.clientWidth,\n" +
+        "      height: document.documentElement.clientHeight,\n" +
+        "      deviceScaleFactor: window.devicePixelRatio\n" +
+        "  }\n" +
+        "}");
+    System.out.println(dimensions);
+    browser.close();
+    playwright.close();
+  }
+}
---
+++ examples/src/main/java/org/example/PageScreenshot.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example;
+
+import com.microsoft.playwright.*;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class PageScreenshot {
+  public static void main(String[] args) throws Exception {
+    Playwright playwright = Playwright.create();
+    List<BrowserType> browserTypes = Arrays.asList(
+        playwright.chromium(),
+        playwright.webkit(),
+        playwright.firefox()
+    );
+    for (BrowserType browserType : browserTypes) {
+      Browser browser = browserType.launch();
+      BrowserContext context = browser.newContext(
+          new Browser.NewContextOptions().withViewport(800, 600));
+      Page page = context.newPage();
+      page.navigate("http://whatsmyuseragent.org/");
+      page.screenshot(new Page.ScreenshotOptions().withPath(Paths.get("screenshot-" + browserType.name() + ".png")));
+      browser.close();
+    }
+    playwright.close();
+  }
+}
