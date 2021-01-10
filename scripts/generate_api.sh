14a2ddc725693a8f79f4186f0be903e22905fcbc
codeboyzhou <imzhouchen@gmail.com>
2021-01-10 14:16:41 +0800

    fix(scripts): 'generate_api.sh' does not work on macOS

--- scripts/generate_api.sh
+++ scripts/generate_api.sh
@@ -3,10 +3,19 @@
 set -e
 set +x
 
-trap "cd $(pwd -P)" EXIT
-cd "$(dirname $0)/.."
+trap 'cd $(pwd -P)' EXIT
+cd "$(dirname "$0")/.."
 
-PLAYWRIGHT_CLI=./driver-bundle/src/main/resources/driver/linux/playwright.sh
+DRIVER_PATH=./driver-bundle/src/main/resources/driver
+case $(uname) in
+Darwin)
+  PLAYWRIGHT_CLI=$DRIVER_PATH/mac/playwright.sh
+  ;;
+Linux|MINGW32*|MINGW64*)
+  PLAYWRIGHT_CLI=$DRIVER_PATH/linux/playwright.sh
+  ;;
+esac
+
 echo "Updating api.json from $($PLAYWRIGHT_CLI --version)"
 
 $PLAYWRIGHT_CLI print-api-json > ./tools/api-generator/src/main/resources/api.json
@@ -14,4 +23,4 @@ $PLAYWRIGHT_CLI print-api-json > ./tools/api-generator/src/main/resources/api.js
 mvn compile -f ./tools/api-generator --no-transfer-progress
 
 echo "Regenerating Java interfaces"
-mvn exec:java --f ./tools/api-generator -Dexec.mainClass=com.microsoft.playwright.tools.ApiGenerator
+mvn exec:java --f ./tools/api-generator -D exec.mainClass=com.microsoft.playwright.tools.ApiGenerator
