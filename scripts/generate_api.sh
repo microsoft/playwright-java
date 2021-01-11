cc1057d910e91d1b1c29633d0148c6a12f7ea3af
codeboyzhou <imzhouchen@gmail.com>
2021-01-12 03:31:53 +0800

    fix(scripts): 'generate_api.sh' does not work on macOS (#201)
    Co-authored-by: Yury Semikhatsky <yurys@chromium.org>

--- scripts/generate_api.sh
+++ scripts/generate_api.sh
@@ -3,10 +3,29 @@
 set -e
 set +x
 
-trap "cd $(pwd -P)" EXIT
-cd "$(dirname $0)/.."
+trap 'cd $(pwd -P)' EXIT
+cd "$(dirname "$0")/.."
 
-PLAYWRIGHT_CLI=./driver-bundle/src/main/resources/driver/linux/playwright.sh
+PLAYWRIGHT_CLI="unknown"
+case $(uname) in
+Darwin)
+  PLAYWRIGHT_CLI=./driver-bundle/src/main/resources/driver/mac/playwright.sh
+  ;;
+Linux)
+  PLAYWRIGHT_CLI=./driver-bundle/src/main/resources/driver/linux/playwright.sh
+  ;;
+MINGW32*)
+  PLAYWRIGHT_CLI=./driver-bundle/src/main/resources/driver/win32/playwright.cmd
+  ;;
+MINGW64*)
+  PLAYWRIGHT_CLI=./driver-bundle/src/main/resources/driver/win32_x64/playwright.cmd
+  ;;
+*)
+  echo "Unknown platform '$(uname)'"
+  exit 1;
+  ;;
+esac
+
 echo "Updating api.json from $($PLAYWRIGHT_CLI --version)"
 
 $PLAYWRIGHT_CLI print-api-json > ./tools/api-generator/src/main/resources/api.json
@@ -14,4 +33,4 @@ $PLAYWRIGHT_CLI print-api-json > ./tools/api-generator/src/main/resources/api.js
 mvn compile -f ./tools/api-generator --no-transfer-progress
 
 echo "Regenerating Java interfaces"
-mvn exec:java --f ./tools/api-generator -Dexec.mainClass=com.microsoft.playwright.tools.ApiGenerator
+mvn exec:java --f ./tools/api-generator -D exec.mainClass=com.microsoft.playwright.tools.ApiGenerator
