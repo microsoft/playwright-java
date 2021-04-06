643abbb24ed91769d570959b474424a1727af3fc
Yury Semikhatsky <yurys@chromium.org>
2021-04-06 16:05:13 -0700

    feat: support connectOverCDP

---
+++ playwright/src/test/java/com/microsoft/playwright/TestBrowserTypeBasic.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.microsoft.playwright;
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.microsoft.playwright.Utils.getBrowserNameFromEnv;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestBrowserTypeBasic extends TestBase {
+  @Test
+  void browserTypeExecutablePathShouldWork() {
+    Assumptions.assumeTrue(getBrowserChannelFromEnv() == null);
+    Assumptions.assumeTrue(createLaunchOptions().executablePath == null, "Skip with custom executable path");
+    String executablePath = browserType.executablePath();
+    assertTrue(Files.exists(Paths.get(executablePath)));
+  }
+
+  @Test
+  void browserTypeNameShouldWork() {
+    assertEquals(getBrowserNameFromEnv(), browserType.name());
+  }
+
+  @Test
+  @DisabledIf(value="com.microsoft.playwright.TestBase#isChromium", disabledReason="Non-chromium behavior")
+  void shouldThrowWhenTryingToConnectWithNotChromium() {
+    try {
+      browserType.connectOverCDP("foo");
+      fail("did not throw");
+    } catch (PlaywrightException e) {
+      assertTrue(e.getMessage().contains("Connecting over CDP is only supported in Chromium."));
+    }
+  }
+}
