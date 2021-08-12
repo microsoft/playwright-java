ebf9b09c3447cf2f07c2fb68df550ce70b31a605
Yury Semikhatsky <yurys@chromium.org>
2021-08-12 08:43:51 -0700

    devops: check that browser versions in README are up to date (#553)

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -7,10 +7,8 @@ trap "cd $(pwd -P)" EXIT
 
 cd "$(dirname $0)/.."
 
-./scripts/generate_api.sh
-
 # Built from source and do local install.
-mvn clean install --no-transfer-progress -D skipTests
+mvn install --no-transfer-progress -D skipTests
 
 echo "Updating browser versions in README.md"
 mvn compile exec:java --f ./tools/update-docs-version -D exec.mainClass=com.microsoft.playwright.tools.UpdateBrowserVersions
