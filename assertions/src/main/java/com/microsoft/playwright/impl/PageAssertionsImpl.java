45b141811b3dd0d8f03ec052cdf44e9e5288e82c
Yury Semikhatsky <yurys@chromium.org>
2021-10-26 17:53:09 -0700

    chore: extract common routines to base class (#666)

--- assertions/src/main/java/com/microsoft/playwright/impl/PageAssertionsImpl.java
+++ assertions/src/main/java/com/microsoft/playwright/impl/PageAssertionsImpl.java
@@ -18,31 +18,30 @@ package com.microsoft.playwright.impl;
 
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.assertions.PageAssertions;
-import org.opentest4j.AssertionFailedError;
 
 import java.util.regex.Pattern;
 
 import static com.microsoft.playwright.impl.UrlMatcher.resolveUrl;
+import static com.microsoft.playwright.impl.Utils.convertViaJson;
 import static java.util.Arrays.asList;
 
-public class PageAssertionsImpl implements PageAssertions {
-  private final PageImpl actual;
-  private final boolean isNot;
+public class PageAssertionsImpl extends AssertionsBase implements PageAssertions {
+  private final PageImpl actualPage;
 
   public PageAssertionsImpl(Page page) {
     this(page, false);
   }
 
   private PageAssertionsImpl(Page page, boolean isNot) {
-    this.actual = (PageImpl) page;
-    this.isNot = isNot;
+    super((LocatorImpl) page.locator(":root"), isNot);
+    this.actualPage = (PageImpl) page;
   }
 
   @Override
   public void hasTitle(String title, HasTitleOptions options) {
     ExpectedTextValue expected = new ExpectedTextValue();
     expected.string = title;
-    expectImpl("to.have.title", expected, title, "Page title expected to be", options == null ? null : options.timeout);
+    expectImpl("to.have.title", expected, title, "Page title expected to be", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -51,17 +50,17 @@ public class PageAssertionsImpl implements PageAssertions {
     ExpectedTextValue expected = new ExpectedTextValue();
     expected.regexSource = pattern.pattern();
     // expected.regexFlags =
-    expectImpl("to.have.title", expected, pattern, "Page title expected to match regex", options == null ? null : options.timeout);
+    expectImpl("to.have.title", expected, pattern, "Page title expected to match regex", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void hasURL(String url, HasURLOptions options) {
     ExpectedTextValue expected = new ExpectedTextValue();
-    if (actual.context().baseUrl != null) {
-      url = resolveUrl(actual.context().baseUrl, url);
+    if (actualPage.context().baseUrl != null) {
+      url = resolveUrl(actualPage.context().baseUrl, url);
     }
     expected.string = url;
-    expectImpl("to.have.url", expected, url, "Page URL expected to be", options == null ? null : options.timeout);
+    expectImpl("to.have.url", expected, url, "Page URL expected to be", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -70,31 +69,12 @@ public class PageAssertionsImpl implements PageAssertions {
     ExpectedTextValue expected = new ExpectedTextValue();
     expected.regexSource = pattern.pattern();
     // expected.regexFlags =
-    expectImpl("to.have.url", expected, pattern, "Page URL expected to match regex", options == null ? null : options.timeout);
-  }
-
-  private void expectImpl(String expression, ExpectedTextValue textValue, Object expected, String message, Double timeout) {
-    LocatorImpl locator = actual.locator(":root");
-    FrameExpectOptions expectOptions = new FrameExpectOptions();
-    expectOptions.expectedText = asList(textValue);
-    expectOptions.isNot = isNot;
-    expectOptions.timeout = timeout;
-    FrameExpectResult result = locator.expect(expression, expectOptions);
-    if (result.matches == isNot) {
-      Object actual = result.received == null ? null : Serialization.deserialize(result.received);
-      String log = String.join("\n", result.log);
-      if (!log.isEmpty()) {
-        log = "\nCall log:\n" + log;
-      }
-      throw new AssertionFailedError(message + log, expected, actual);
-    }
+    expectImpl("to.have.url", expected, pattern, "Page URL expected to match regex", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public PageAssertions not() {
-    return new PageAssertionsImpl(actual, !isNot);
+    return new PageAssertionsImpl(actualPage, !isNot);
   }
-
-
 }
 
