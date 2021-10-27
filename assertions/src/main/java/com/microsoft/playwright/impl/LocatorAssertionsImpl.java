3dd2d46b5178caa08deb6e18890cebb6985a32e4
Yury Semikhatsky <yurys@chromium.org>
2021-10-26 17:14:40 -0700

    chore: extract common routines to base class

--- assertions/src/main/java/com/microsoft/playwright/impl/LocatorAssertionsImpl.java
+++ assertions/src/main/java/com/microsoft/playwright/impl/LocatorAssertionsImpl.java
@@ -18,36 +18,21 @@ package com.microsoft.playwright.impl;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.assertions.LocatorAssertions;
-import org.opentest4j.AssertionFailedError;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import static com.microsoft.playwright.impl.AssertionUtils.formatValue;
 import static com.microsoft.playwright.impl.Serialization.serializeArgument;
 import static com.microsoft.playwright.impl.Utils.convertViaJson;
-import static java.util.Arrays.asList;
 
-public class LocatorAssertionsImpl implements LocatorAssertions {
-  private final LocatorImpl actual;
-  private final boolean isNot;
-
-  static class HasTextCommonOptions {
-    public Object expressionArg;
-    public SerializedArgument expectedValue;
-    public Double timeout;
-    public Boolean useInnerText;
-    public Integer expectedNumber;
-  }
-
+public class LocatorAssertionsImpl extends AssertionsBase implements LocatorAssertions {
   public LocatorAssertionsImpl(Locator locator) {
     this(locator, false);
   }
 
   private LocatorAssertionsImpl(Locator locator, boolean isNot) {
-    this.actual = (LocatorImpl) locator;
-    this.isNot = isNot;
+    super((LocatorImpl) locator, isNot);
   }
 
   @Override
@@ -56,7 +41,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
     expected.string = text;
     expected.matchSubstring = true;
     expected.normalizeWhiteSpace = true;
-    expectImpl("to.have.text", expected, text, "Locator expected to contain text", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.text", expected, text, "Locator expected to contain text", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -66,7 +51,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
     // expected.regexFlags =
     expected.matchSubstring = true;
     expected.normalizeWhiteSpace = true;
-    expectImpl("to.have.text", expected, pattern, "Locator expected to contain text", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.text", expected, pattern, "Locator expected to contain text", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -79,7 +64,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
       expected.normalizeWhiteSpace = true;
       list.add(expected);
     }
-    expectImpl("to.contain.text.array", list, strings, "Locator expected to contain text", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.contain.text.array", list, strings, "Locator expected to contain text", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -92,7 +77,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
       expected.normalizeWhiteSpace = true;
       list.add(expected);
     }
-    expectImpl("to.contain.text.array", list, patterns, "Locator expected to contain text", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.contain.text.array", list, patterns, "Locator expected to contain text", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -113,7 +98,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
     if (options == null) {
       options = new HasAttributeOptions();
     }
-    HasTextCommonOptions commonOptions = convertViaJson(options, HasTextCommonOptions.class);
+    FrameExpectOptions commonOptions = convertViaJson(options, FrameExpectOptions.class);
     commonOptions.expressionArg = name;
     expectImpl("to.have.attribute", expectedText, expectedValue, "Locator expected to have attribute", commonOptions);
   }
@@ -122,14 +107,14 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
   public void hasClass(String text, HasClassOptions options) {
     ExpectedTextValue expected = new ExpectedTextValue();
     expected.string = text;
-    expectImpl("to.have.class", expected, text, "Locator expected to have class", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.class", expected, text, "Locator expected to have class", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void hasClass(Pattern pattern, HasClassOptions options) {
     ExpectedTextValue expected = new ExpectedTextValue();
     expected.regexSource = pattern.pattern();
-    expectImpl("to.have.class", expected, pattern, "Locator expected to have class", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.class", expected, pattern, "Locator expected to have class", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -140,7 +125,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
       expected.string = text;
       list.add(expected);
     }
-    expectImpl("to.have.class.array", list, strings, "Locator expected to have class", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.class.array", list, strings, "Locator expected to have class", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -151,7 +136,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
       expected.regexSource = pattern.pattern();
       list.add(expected);
     }
-    expectImpl("to.have.class.array", list, patterns, "Locator expected to have class", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.class.array", list, patterns, "Locator expected to have class", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -159,7 +144,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
     if (options == null) {
       options = new HasCountOptions();
     }
-    HasTextCommonOptions commonOptions = convertViaJson(options, HasTextCommonOptions.class);
+    FrameExpectOptions commonOptions = convertViaJson(options, FrameExpectOptions.class);
     commonOptions.expectedNumber = count;
     List<ExpectedTextValue> expectedText = null;
     expectImpl("to.have.count", expectedText, count, "Locator expected to have count", commonOptions);
@@ -183,7 +168,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
     if (options == null) {
       options = new HasCSSOptions();
     }
-    HasTextCommonOptions commonOptions = convertViaJson(options, HasTextCommonOptions.class);
+    FrameExpectOptions commonOptions = convertViaJson(options, FrameExpectOptions.class);
     commonOptions.expressionArg = name;
     expectImpl("to.have.css", expectedText, expectedValue, "Locator expected to have CSS", commonOptions);
   }
@@ -192,7 +177,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
   public void hasId(String id, HasIdOptions options) {
     ExpectedTextValue expected = new ExpectedTextValue();
     expected.string = id;
-    expectImpl("to.have.id", expected, id, "Locator expected to have ID", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.id", expected, id, "Locator expected to have ID", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -200,7 +185,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
     if (options == null) {
       options = new HasJSPropertyOptions();
     }
-    HasTextCommonOptions commonOptions = convertViaJson(options, HasTextCommonOptions.class);
+    FrameExpectOptions commonOptions = convertViaJson(options, FrameExpectOptions.class);
     commonOptions.expressionArg = name;
     commonOptions.expectedValue = serializeArgument(value);
     List<ExpectedTextValue> list = null;
@@ -213,7 +198,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
     expected.string = text;
     expected.matchSubstring = false;
     expected.normalizeWhiteSpace = true;
-    expectImpl("to.have.text", expected, text, "Locator expected to have text", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.text", expected, text, "Locator expected to have text", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -223,7 +208,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
     // Just match substring, same as containsText.
     expected.matchSubstring = true;
     expected.normalizeWhiteSpace = true;
-    expectImpl("to.have.text", expected, pattern, "Locator expected to have text", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.text", expected, pattern, "Locator expected to have text", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -236,7 +221,7 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
       expected.normalizeWhiteSpace = true;
       list.add(expected);
     }
-    expectImpl("to.have.text.array", list, strings, "Locator expected to have text", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.text.array", list, strings, "Locator expected to have text", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
@@ -249,105 +234,71 @@ public class LocatorAssertionsImpl implements LocatorAssertions {
       expected.normalizeWhiteSpace = true;
       list.add(expected);
     }
-    expectImpl("to.have.text.array", list, patterns, "Locator expected to have text", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.text.array", list, patterns, "Locator expected to have text", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void hasValue(String value, HasValueOptions options) {
     ExpectedTextValue expected = new ExpectedTextValue();
     expected.string = value;
-    expectImpl("to.have.value", expected, value, "Locator expected to have value", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.value", expected, value, "Locator expected to have value", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void hasValue(Pattern pattern, HasValueOptions options) {
     ExpectedTextValue expected = new ExpectedTextValue();
     expected.regexSource = pattern.pattern();
-    expectImpl("to.have.value", expected, pattern, "Locator expected to have value", convertViaJson(options, HasTextCommonOptions.class));
+    expectImpl("to.have.value", expected, pattern, "Locator expected to have value", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void isChecked(IsCheckedOptions options) {
-    expectTrue("to.be.checked", "Locator expected to be checked", convertViaJson(options, HasTextCommonOptions.class));
+    expectTrue("to.be.checked", "Locator expected to be checked", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void isDisabled(IsDisabledOptions options) {
-    expectTrue("to.be.disabled", "Locator expected to be disabled", convertViaJson(options, HasTextCommonOptions.class));
+    expectTrue("to.be.disabled", "Locator expected to be disabled", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void isEditable(IsEditableOptions options) {
-    expectTrue("to.be.editable", "Locator expected to be editable", convertViaJson(options, HasTextCommonOptions.class));
+    expectTrue("to.be.editable", "Locator expected to be editable", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void isEmpty(IsEmptyOptions options) {
-    expectTrue("to.be.empty", "Locator expected to be empty", convertViaJson(options, HasTextCommonOptions.class));
+    expectTrue("to.be.empty", "Locator expected to be empty", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void isEnabled(IsEnabledOptions options) {
-    expectTrue("to.be.enabled", "Locator expected to be enabled", convertViaJson(options, HasTextCommonOptions.class));
+    expectTrue("to.be.enabled", "Locator expected to be enabled", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void isFocused(IsFocusedOptions options) {
-    expectTrue("to.be.focused", "Locator expected to be focused", convertViaJson(options, HasTextCommonOptions.class));
+    expectTrue("to.be.focused", "Locator expected to be focused", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void isHidden(IsHiddenOptions options) {
-    expectTrue("to.be.hidden", "Locator expected to be hidden", convertViaJson(options, HasTextCommonOptions.class));
+    expectTrue("to.be.hidden", "Locator expected to be hidden", convertViaJson(options, FrameExpectOptions.class));
   }
 
   @Override
   public void isVisible(IsVisibleOptions options) {
-    expectTrue("to.be.visible", "Locator expected to be visible", convertViaJson(options, HasTextCommonOptions.class));
+    expectTrue("to.be.visible", "Locator expected to be visible", convertViaJson(options, FrameExpectOptions.class));
   }
 
-  private void expectImpl(String expression, ExpectedTextValue textValue, Object expected, String message, HasTextCommonOptions options) {
-    expectImpl(expression, asList(textValue), expected, message, options);
-  }
-
-  private void expectTrue(String expression, String message, HasTextCommonOptions options) {
+  private void expectTrue(String expression, String message, FrameExpectOptions options) {
     List<ExpectedTextValue> expectedText = null;
     expectImpl(expression, expectedText, null, message, options);
   }
 
-  private void expectImpl(String expression, List<ExpectedTextValue> expectedText, Object expected, String message, HasTextCommonOptions options) {
-    if (options == null) {
-      options = new HasTextCommonOptions();
-    }
-    FrameExpectOptions expectOptions = new FrameExpectOptions();
-    expectOptions.expressionArg = options.expressionArg;
-    expectOptions.expectedNumber = options.expectedNumber;
-    expectOptions.expectedValue = options.expectedValue;
-    expectOptions.expectedText = expectedText;
-    expectOptions.isNot = isNot;
-    expectOptions.timeout = options.timeout;
-    expectOptions.useInnerText = options.useInnerText;
-    expectImpl(expression, expectOptions, expected, message);
-  }
-
-  private void expectImpl(String expression, FrameExpectOptions expectOptions, Object expected, String message) {
-    FrameExpectResult result = actual.expect(expression, expectOptions);
-    if (result.matches == isNot) {
-      Object actual = result.received == null ? null : Serialization.deserialize(result.received);
-      String log = String.join("\n", result.log);
-      if (!log.isEmpty()) {
-        log = "\nCall log:\n" + log;
-      }
-      if (expected == null) {
-        throw new AssertionFailedError(message + log);
-      }
-      throw new AssertionFailedError(message + log, formatValue(expected), formatValue(actual));
-    }
-  }
-
   @Override
   public LocatorAssertions not() {
-    return new LocatorAssertionsImpl(actual, !isNot);
+    return new LocatorAssertionsImpl(actualLocator, !isNot);
   }
 }
 
