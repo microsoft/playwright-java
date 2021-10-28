ac86ae16444276e281f208307c687a3466e732f3
Yury Semikhatsky <yurys@chromium.org>
2021-10-28 16:31:27 -0700

    upload surefire-reports

--- tools/test-local-installation/create_project_and_run_tests.sh
+++ tools/test-local-installation/create_project_and_run_tests.sh
@@ -7,6 +7,7 @@ trap "cd $(pwd -P)" EXIT
 cd "$(dirname $0)"
 
 
+LOG_DIR=$(pwd)/../../logs
 PROJECT_DIR=$(mktemp -d)
 echo "Creating project in $PROJECT_DIR"
 cp -R . $PROJECT_DIR
@@ -15,6 +16,8 @@ cp -R ../../driver-bundle/src/test/ $PROJECT_DIR/src/
 cp -R ../../playwright/src/test/ $PROJECT_DIR/src/
 cd $PROJECT_DIR
 
-mvn test --no-transfer-progress -D test=*Test*Route*
+mvn test --no-transfer-progress -D test=*TestPageRoute*
+
+cp -R target/surefire-reports $LOG_DIR
 
 rm -rf $PROJECT_DIR
