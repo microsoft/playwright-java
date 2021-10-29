ba752dc82239c019fd40f6244545e4e342c5d8d0
Yury Semikhatsky <yurys@chromium.org>
2021-10-29 11:21:01 -0700

    do not upload artifacts

--- tools/test-local-installation/create_project_and_run_tests.sh
+++ tools/test-local-installation/create_project_and_run_tests.sh
@@ -7,7 +7,6 @@ trap "cd $(pwd -P)" EXIT
 cd "$(dirname $0)"
 
 
-LOG_DIR=$(pwd)/../../logs
 PROJECT_DIR=$(mktemp -d)
 echo "Creating project in $PROJECT_DIR"
 cp -R . $PROJECT_DIR
@@ -17,7 +16,5 @@ cp -R ../../playwright/src/test/ $PROJECT_DIR/src/
 cd $PROJECT_DIR
 
 mvn test --no-transfer-progress -D test=*Test*Route*
-
-cp -R target/surefire-reports $LOG_DIR
 
 rm -rf $PROJECT_DIR
