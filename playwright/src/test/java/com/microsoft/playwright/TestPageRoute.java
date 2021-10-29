2ee025bd561e41f97a7706b351c16671e2f27d69
Yury Semikhatsky <yurys@chromium.org>
2021-10-28 18:13:59 -0700

    test w/ just stderr

--- playwright/src/test/java/com/microsoft/playwright/TestPageRoute.java
+++ playwright/src/test/java/com/microsoft/playwright/TestPageRoute.java
@@ -97,7 +97,7 @@ public class TestPageRoute extends TestBase {
 
   @Test
   void shouldWorkWhenPOSTIsRedirectedWith302() {
-    System.out.println("OOO");
+    System.err.println("EEE");
     server.setRedirect("/rredirect", "/empty.html");
     page.navigate(server.EMPTY_PAGE);
     page.route("**/*", route -> route.resume());
