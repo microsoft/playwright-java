f0235e0b58c295128118d85229fe89a806f6ec97
Yury Semikhatsky <yurys@chromium.org>
2021-11-30 11:47:18 -0800

    chore: remove unused script

--- scripts/playwright.java
+++ scripts/playwright.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS com.microsoft.playwright:playwright:RELEASE
-//DESCRIPTION Playwright lets you automate Chromium, Firefox and Webkit with a single API. 
+//DESCRIPTION Playwright lets you automate Chromium, Firefox and Webkit with a single API.
 //DESCRIPTION With this cli you can install, trace, generate pdf and screenshots and more.
 //DESCRIPTION
 //DESCRIPTION Example on how to record and run a script:
@@ -9,6 +9,7 @@
 //DESCRIPTION   jbang --deps com.microsoft.playwright:playwright:RELEASE Example.java
 //DESCRIPTION ```
 
+// NOTE: this file is referenced from https://github.com/microsoft/jbang-catalog/blob/702c9b4355c76ae6f7294e1aaf6f2698b03631aa/jbang-catalog.json#L17
 public class playwright {
 
     public static void main(String... args) throws Exception {
