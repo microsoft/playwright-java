447578c5824688e5209dadbb233867ed0b20385c
uchagani <umair.chagani@gmail.com>
2022-04-04 15:25:37 -0400

    fix(driver): set driver instance to null if an exception is thrown du… (#879)

--- driver-bundle/src/test/java/com/microsoft/playwright/TestInstall.java
+++ driver-bundle/src/test/java/com/microsoft/playwright/TestInstall.java
@@ -18,14 +18,14 @@ package com.microsoft.playwright;
 
 import com.microsoft.playwright.impl.Driver;
 import com.microsoft.playwright.impl.DriverJar;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -41,6 +41,16 @@ public class TestInstall {
     System.clearProperty("playwright.driver.tmpdir");
     // Clear system property to ensure that the default driver is loaded.
     System.clearProperty("playwright.driver.impl");
+  }
+
+  @Test
+  @Tags({@Tag("isolated"), @Tag("driverThrowTest")})
+  void shouldThrowWhenBrowserPathIsInvalid() {
+    Map<String,String> env = new HashMap<>();
+    env.put("PLAYWRIGHT_BROWSERS_PATH", "/some/bad/path/that/should/not/exist/i/hope");
+
+    assertThrows(RuntimeException.class, () -> Driver.ensureDriverInstalled(env, true));
+    assertThrows(RuntimeException.class, () -> Driver.ensureDriverInstalled(env, true));
   }
 
   @Test
