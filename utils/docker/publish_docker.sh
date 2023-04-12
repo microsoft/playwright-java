804c57707059f77e00d269d5915146ad6895e693
Max Schmitt <max@schmitt.mx>
2023-04-12 19:41:41 +0200

    devops: publish Jammy Docker images (#1262)

--- utils/docker/publish_docker.sh
+++ utils/docker/publish_docker.sh
@@ -34,14 +34,6 @@ if [[ -z "${GITHUB_SHA}" ]]; then
   exit 1
 fi
 
-BIONIC_TAGS=(
-  "next-bionic"
-  "v${PW_VERSION}-bionic"
-)
-if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
-  BIONIC_TAGS+=("bionic")
-fi
-
 FOCAL_TAGS=(
   "next"
   "sha-${GITHUB_SHA}"
@@ -55,6 +47,15 @@ if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
   FOCAL_TAGS+=("v${PW_VERSION}")
 fi
 
+JAMMY_TAGS=(
+  "next-jammy"
+  "v${PW_VERSION}-jammy"
+)
+
+if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
+  JAMMY_TAGS+=("jammy")
+fi
+
 tag_and_push() {
   local source="$1"
   local target="$2"
@@ -66,12 +67,12 @@ tag_and_push() {
 publish_docker_images_with_arch_suffix() {
   local FLAVOR="$1"
   local TAGS=()
-  if [[ "$FLAVOR" == "bionic" ]]; then
-    TAGS=("${BIONIC_TAGS[@]}")
-  elif [[ "$FLAVOR" == "focal" ]]; then
+  if [[ "$FLAVOR" == "focal" ]]; then
     TAGS=("${FOCAL_TAGS[@]}")
+  elif [[ "$FLAVOR" == "jammy" ]]; then
+    TAGS=("${JAMMY_TAGS[@]}")
   else
-    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic' or 'focal'"
+    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'focal' or 'jammy'"
     exit 1
   fi
   local ARCH="$2"
@@ -92,12 +93,12 @@ publish_docker_images_with_arch_suffix() {
 publish_docker_manifest () {
   local FLAVOR="$1"
   local TAGS=()
-  if [[ "$FLAVOR" == "bionic" ]]; then
-    TAGS=("${BIONIC_TAGS[@]}")
-  elif [[ "$FLAVOR" == "focal" ]]; then
+  if [[ "$FLAVOR" == "focal" ]]; then
     TAGS=("${FOCAL_TAGS[@]}")
+  elif [[ "$FLAVOR" == "jammy" ]]; then
+    TAGS=("${JAMMY_TAGS[@]}")
   else
-    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'bionic' or 'focal'"
+    echo "ERROR: unknown flavor - $FLAVOR. Must be either 'focal' or 'jammy'"
     exit 1
   fi
 
@@ -119,3 +120,7 @@ publish_docker_manifest () {
 publish_docker_images_with_arch_suffix focal amd64
 publish_docker_images_with_arch_suffix focal arm64
 publish_docker_manifest focal amd64 arm64
+
+publish_docker_images_with_arch_suffix jammy amd64
+publish_docker_images_with_arch_suffix jammy arm64
+publish_docker_manifest jammy amd64 arm64
