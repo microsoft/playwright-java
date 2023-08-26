12962568d8b451b4f11c062f0f99a1e3ea887fb7
jfgreffier <jfgreffier@users.noreply.github.com>
2023-08-26 15:50:08 +0200

    feat: junit base classes for Playwright

---
+++ playwright/src/main/java/com/microsoft/playwright/junit/BrowserTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.microsoft.playwright.junit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+
+public class BrowserTest extends PlaywrightTest {
+  public Browser browser;
+  public String browserName;
+
+  @BeforeAll
+  void launchBrowser() {
+    BrowserType browserType;
+    if ("firefox".equals(PlaywrightSettings.browserName())) {
+      browserType = playwright.firefox();
+    } else if ("webkit".equals(PlaywrightSettings.browserName())) {
+      browserType = playwright.webkit();
+    } else {
+      browserType = playwright.chromium();
+    }
+    browserName = browserType.name();
+
+    browser = browserType.launch(new BrowserType.LaunchOptions()
+        .setHeadless(PlaywrightSettings.headless()));
+  }
+
+  @AfterAll
+  void closeBrowser() {
+    browser.close();
+  }
+}
---
+++ playwright/src/main/java/com/microsoft/playwright/junit/PlaywrightSettings.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.microsoft.playwright.junit;
+
+public class PlaywrightSettings {
+  static boolean headless() {
+    if ("1".equals(System.getenv("HEADED"))) {
+      return false;
+    }
+    return true;
+  }
+
+  static String browserName() {
+    if ("firefox".equals(System.getenv("BROWSER"))) {
+      return "firefox";
+    }
+    if ("webkit".equals(System.getenv("BROWSER"))) {
+      return "webkit";
+    }
+    if ("chromium".equals(System.getenv("BROWSER"))) {
+      return "chromium";
+    }
+    return "chromium";
+  };
+}
