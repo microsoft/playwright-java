167ed97bee408e0f7a49267bd58abe1c33044d21
Umair Chagani <umair.chagani@gmail.com>
2023-09-04 16:53:24 -0400

    feat(junit-playwright): cleanup

--- playwright-junit/src/main/java/com/microsoft/playwright/JUnitUtils.java
+++ playwright-junit/src/main/java/com/microsoft/playwright/JUnitUtils.java
@@ -11,10 +11,12 @@ import static org.junit.platform.commons.support.AnnotationSupport.findAnnotatio
 class JUnitUtils {
   // Necessary because of https://github.com/junit-team/junit5/issues/3447
   static ExecutionMode getExecutionMode(ExtensionContext extensionContext) {
-    return findAnnotation(extensionContext.getTestClass(), Execution.class)
+    return findAnnotation(extensionContext.getTestMethod(), Execution.class)
       .map(Execution::value)
-      .orElseGet(() -> extensionContext.getConfigurationParameter("junit.jupiter.execution.parallel.mode.default")
-        .map(paramValue -> ExecutionMode.valueOf(paramValue.toUpperCase(Locale.ROOT)))
-        .orElse(ExecutionMode.SAME_THREAD));
+      .orElseGet(() -> findAnnotation(extensionContext.getTestClass(), Execution.class)
+        .map(Execution::value)
+        .orElseGet(() -> extensionContext.getConfigurationParameter("junit.jupiter.execution.parallel.mode.default")
+          .map(paramValue -> ExecutionMode.valueOf(paramValue.toUpperCase(Locale.ROOT)))
+          .orElse(ExecutionMode.SAME_THREAD)));
   }
 }
