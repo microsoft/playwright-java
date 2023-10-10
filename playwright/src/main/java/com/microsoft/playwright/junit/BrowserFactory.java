7dab27253e7d9095adfd92690f02546e26774fc2
Yury Semikhatsky <yurys@chromium.org>
2023-10-10 16:21:49 -0700

    Revert "feat(junit-playwright) (#1371)"
    This reverts commit fb2188cd2a041768be664f899d32b856787a7b73.

--- playwright/src/main/java/com/microsoft/playwright/junit/BrowserFactory.java
+++
@@ -1,8 +0,0 @@
-package com.microsoft.playwright.junit;
-
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.Playwright;
-
-public interface BrowserFactory {
-  Browser newBrowser(Playwright playwright);
-}
