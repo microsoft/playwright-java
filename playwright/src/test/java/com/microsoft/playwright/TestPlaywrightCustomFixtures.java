a0cd4992980057dd2a2b9954a695c4ebe59208b0
Umair Chagani <umair.chagani@gmail.com>
2024-01-21 10:13:10 -0500

    Created an extension to handle server lifecycle instead of duplicating code

--- playwright/src/test/java/com/microsoft/playwright/TestPlaywrightCustomFixtures.java
+++ playwright/src/test/java/com/microsoft/playwright/TestPlaywrightCustomFixtures.java
@@ -3,38 +3,22 @@ package com.microsoft.playwright;
 import com.microsoft.playwright.junit.Options;
 import com.microsoft.playwright.junit.OptionsFactory;
 import com.microsoft.playwright.junit.UsePlaywright;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.util.regex.Pattern;
 
-import static com.microsoft.playwright.Utils.nextFreePort;
+import static com.microsoft.playwright.ServerLifecycle.serverMap;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@FixtureTest
 @UsePlaywright(TestPlaywrightCustomFixtures.CustomOptions.class)
 public class TestPlaywrightCustomFixtures {
-  private static Server server;
 
   public static class CustomOptions implements OptionsFactory {
     @Override
     public Options getOptions() {
-      return new Options().setBaseUrl(server.EMPTY_PAGE).setBrowserName("firefox");
-    }
-  }
-
-  @BeforeAll
-  static void beforeAll() throws IOException {
-    server = Server.createHttp(nextFreePort());
-  }
-
-  @AfterAll
-  static void afterAll() {
-    if (server != null) {
-      server.stop();
-      server = null;
+      return new Options().setBaseUrl(serverMap.get(TestPlaywrightCustomFixtures.class).EMPTY_PAGE).setBrowserName("firefox");
     }
   }
 
--- playwright/src/test/java/com/microsoft/playwright/TestPlaywrightCustomOptionFixtures.java
+++ playwright/src/test/java/com/microsoft/playwright/TestPlaywrightCustomOptionFixtures.java
@@ -3,38 +3,21 @@ package com.microsoft.playwright;
 import com.microsoft.playwright.junit.Options;
 import com.microsoft.playwright.junit.OptionsFactory;
 import com.microsoft.playwright.junit.UsePlaywright;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.util.regex.Pattern;
 
-import static com.microsoft.playwright.Utils.nextFreePort;
+import static com.microsoft.playwright.ServerLifecycle.serverMap;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@FixtureTest
 @UsePlaywright(TestPlaywrightCustomOptionFixtures.CustomOptions.class)
 public class TestPlaywrightCustomOptionFixtures {
-  private static Server server;
-
   public static class CustomOptions implements OptionsFactory {
     @Override
     public Options getOptions() {
-      return new Options().setChannel("chrome").setApiRequestOptions(new APIRequest.NewContextOptions().setBaseURL(server.EMPTY_PAGE)).setContextOption(new Browser.NewContextOptions().setBaseURL(server.EMPTY_PAGE));
-    }
-  }
-
-  @BeforeAll
-  static void beforeAll() throws IOException {
-    server = Server.createHttp(nextFreePort());
-  }
-
-  @AfterAll
-  static void afterAll() {
-    if (server != null) {
-      server.stop();
-      server = null;
+      return new Options().setChannel("chrome").setApiRequestOptions(new APIRequest.NewContextOptions().setBaseURL(serverMap.get(TestPlaywrightCustomOptionFixtures.class).EMPTY_PAGE)).setContextOption(new Browser.NewContextOptions().setBaseURL(serverMap.get(TestPlaywrightCustomOptionFixtures.class).EMPTY_PAGE));
     }
   }
 
