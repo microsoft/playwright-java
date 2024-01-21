198afc34a61f49667874095edca9e16e58e8eb6c
Umair Chagani <umair.chagani@gmail.com>
2024-01-20 21:00:26 -0500

    Use local url for tests

--- playwright/src/test/java/com/microsoft/playwright/TestPlaywrightCustomFixtures.java
+++ playwright/src/test/java/com/microsoft/playwright/TestPlaywrightCustomFixtures.java
@@ -3,19 +3,38 @@ package com.microsoft.playwright;
 import com.microsoft.playwright.junit.Options;
 import com.microsoft.playwright.junit.OptionsFactory;
 import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.regex.Pattern;
 
+import static com.microsoft.playwright.Utils.nextFreePort;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @UsePlaywright(TestPlaywrightCustomFixtures.CustomOptions.class)
 public class TestPlaywrightCustomFixtures {
+  private static Server server;
+
   public static class CustomOptions implements OptionsFactory {
     @Override
     public Options getOptions() {
-      return new Options().setBaseUrl("https://bing.com").setBrowserName("firefox");
+      return new Options().setBaseUrl(server.EMPTY_PAGE).setBrowserName("firefox");
+    }
+  }
+
+  @BeforeAll
+  static void beforeAll() throws IOException {
+    server = Server.createHttp(nextFreePort());
+  }
+
+  @AfterAll
+  static void afterAll() {
+    if (server != null) {
+      server.stop();
+      server = null;
     }
   }
 
@@ -27,6 +46,6 @@ public class TestPlaywrightCustomFixtures {
   @Test
   public void testBaseUrl(Page page) {
     page.navigate("/");
-    assertThat(page).hasURL(Pattern.compile("bing"));
+    assertThat(page).hasURL(Pattern.compile("localhost"));
   }
 }
--- playwright/src/test/java/com/microsoft/playwright/TestPlaywrightCustomOptionFixtures.java
+++ playwright/src/test/java/com/microsoft/playwright/TestPlaywrightCustomOptionFixtures.java
@@ -3,31 +3,50 @@ package com.microsoft.playwright;
 import com.microsoft.playwright.junit.Options;
 import com.microsoft.playwright.junit.OptionsFactory;
 import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.regex.Pattern;
 
+import static com.microsoft.playwright.Utils.nextFreePort;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @UsePlaywright(TestPlaywrightCustomOptionFixtures.CustomOptions.class)
 public class TestPlaywrightCustomOptionFixtures {
+  private static Server server;
+
   public static class CustomOptions implements OptionsFactory {
     @Override
     public Options getOptions() {
-      return new Options().setChannel("chrome").setApiRequestOptions(new APIRequest.NewContextOptions().setBaseURL("https://bing.com")).setContextOption(new Browser.NewContextOptions().setBaseURL("https://microsoft.com"));
+      return new Options().setChannel("chrome").setApiRequestOptions(new APIRequest.NewContextOptions().setBaseURL(server.EMPTY_PAGE)).setContextOption(new Browser.NewContextOptions().setBaseURL(server.EMPTY_PAGE));
+    }
+  }
+
+  @BeforeAll
+  static void beforeAll() throws IOException {
+    server = Server.createHttp(nextFreePort());
+  }
+
+  @AfterAll
+  static void afterAll() {
+    if (server != null) {
+      server.stop();
+      server = null;
     }
   }
 
   @Test
   void testCustomBrowserContext(Page page) {
     page.navigate("/");
-    assertThat(page).hasURL(Pattern.compile("microsoft"));
+    assertThat(page).hasURL(Pattern.compile("localhost"));
   }
 
   @Test
   void testCustomAPIRequestOptions(APIRequestContext apiRequestContext) {
     APIResponse response = apiRequestContext.get("/");
-    assertTrue(response.url().contains("bing"));
+    assertTrue(response.url().contains("localhost"));
   }
 }
