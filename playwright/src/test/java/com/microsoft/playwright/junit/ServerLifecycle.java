458a59ac3bcd9460710102ea229143581ad5a2a5
Umair Chagani <umair.chagani@gmail.com>
2024-02-14 20:17:54 -0500

    Fix tests

--- playwright/src/test/java/com/microsoft/playwright/junit/ServerLifecycle.java
+++ playwright/src/test/java/com/microsoft/playwright/junit/ServerLifecycle.java
@@ -9,13 +9,18 @@ import java.util.Map;
 
 import static com.microsoft.playwright.Utils.nextFreePort;
 
-public class ServerLifecycle implements AfterAllCallback, ParameterResolver {
+public class ServerLifecycle implements BeforeAllCallback, AfterAllCallback, ParameterResolver {
   // This is a public map so that objects outside test scope can access the server.
   // For example, nested classes inside test classes that define custom options and need the server.
   public static Map<Class<?>, Server> serverMap;
 
   static {
     serverMap = new HashMap<>();
+  }
+
+  @Override
+  public void beforeAll(ExtensionContext extensionContext) throws Exception {
+    getOrCreateServer(extensionContext);
   }
 
   @Override
@@ -33,6 +38,10 @@ public class ServerLifecycle implements AfterAllCallback, ParameterResolver {
 
   @Override
   public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return getOrCreateServer(extensionContext);
+  }
+
+  private Server getOrCreateServer(ExtensionContext extensionContext) {
     Server server = serverMap.get(extensionContext.getRequiredTestClass());
     if(server == null) {
       try {
