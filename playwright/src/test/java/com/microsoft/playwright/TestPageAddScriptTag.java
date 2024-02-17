64c5cafba256ed6ff08e64a5c98dcf88c4de4ce3
Yury Semikhatsky <yurys@chromium.org>
2024-02-17 08:54:57 -0800

    Fix shouldIncludeSourceURLWhenPathIsProvided on Windows

--- playwright/src/test/java/com/microsoft/playwright/TestPageAddScriptTag.java
+++ playwright/src/test/java/com/microsoft/playwright/TestPageAddScriptTag.java
@@ -3,6 +3,7 @@ package com.microsoft.playwright;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIf;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -16,6 +17,6 @@ public class TestPageAddScriptTag extends TestBase {
     Path path = Paths.get("src/test/resources/injectedfile.js");
     page.addScriptTag(new Page.AddScriptTagOptions().setPath(path));
     String result = (String) page.evaluate("() => window['__injectedError'].stack");
-    assertTrue(result.contains("resources/injectedfile.js"), result);
+    assertTrue(result.contains("resources" + File.pathSeparator + "injectedfile.js"), result);
   }
 }
