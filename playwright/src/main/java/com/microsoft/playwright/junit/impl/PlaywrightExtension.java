4a07105d8bdc97e1bab6602847766727ec7fedf6
uchagani <umair.chagani@gmail.com>
2024-02-16 21:11:22 -0500

    feat(junit): Close Playwright after test run (#1492)

--- playwright/src/main/java/com/microsoft/playwright/junit/impl/PlaywrightExtension.java
+++ playwright/src/main/java/com/microsoft/playwright/junit/impl/PlaywrightExtension.java
@@ -4,19 +4,52 @@ import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.junit.Options;
 import org.junit.jupiter.api.extension.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static com.microsoft.playwright.junit.impl.ExtensionUtils.isParameterSupported;
 import static com.microsoft.playwright.junit.impl.ExtensionUtils.setTestIdAttribute;
 
-public class PlaywrightExtension implements ParameterResolver, AfterAllCallback {
-  private static final ThreadLocal<Playwright> threadLocalPlaywright = new ThreadLocal<>();
+public class PlaywrightExtension implements ParameterResolver, BeforeAllCallback, ExtensionContext.Store.CloseableResource {
+  private static final ThreadLocal<Playwright> threadLocalPlaywright;
+  private static final List<Playwright> playwrightList;
+  private final static Lock beforeLock;
+  private static boolean isTestRunStarted;
+  private static final ExtensionContext.Namespace namespace = ExtensionContext.Namespace.create(PlaywrightExtension.class);
 
+  static {
+    isTestRunStarted = false;
+    beforeLock = new ReentrantLock();
+    threadLocalPlaywright = new ThreadLocal<>();
+    playwrightList = Collections.synchronizedList(new ArrayList<>());
+  }
+
+  // Before a Test class starts, we register a closeable resource
+  // We use a lock + boolean to ensure this only gets run once for the entire test run
   @Override
-  public void afterAll(ExtensionContext extensionContext) {
-    Playwright playwright = threadLocalPlaywright.get();
-    threadLocalPlaywright.remove();
-    if (playwright != null) {
+  public void beforeAll(final ExtensionContext context) {
+    try {
+      beforeLock.lock();
+      if (!isTestRunStarted) {
+        isTestRunStarted = true;
+        context.getRoot().getStore(namespace).put(context.getUniqueId(), this);
+      }
+    } finally {
+      beforeLock.unlock();
+    }
+  }
+
+  // This is a workaround for JUnit's lack of an "AfterTestRun" hook
+  // This will be called once after all tests have completed.
+  @Override
+  public void close() throws Throwable {
+    for (Playwright playwright : playwrightList) {
       playwright.close();
     }
+    playwrightList.clear();
   }
 
   @Override
@@ -39,6 +72,7 @@ public class PlaywrightExtension implements ParameterResolver, AfterAllCallback 
     Options options = OptionsExtension.getOptions(extensionContext);
     playwright = Playwright.create(options.playwrightCreateOptions);
     threadLocalPlaywright.set(playwright);
+    playwrightList.add(playwright);
     setTestIdAttribute(playwright, options);
     return playwright;
   }
--- playwright/src/test/java/com/microsoft/playwright/junit/ServerLifecycle.java
+++ playwright/src/test/java/com/microsoft/playwright/junit/ServerLifecycle.java
@@ -1,22 +1,26 @@
 package com.microsoft.playwright.junit;
 
 import com.microsoft.playwright.Server;
-import org.junit.jupiter.api.extension.AfterAllCallback;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.microsoft.playwright.Utils.nextFreePort;
 
-public class ServerLifecycle implements BeforeAllCallback, AfterAllCallback {
+public class ServerLifecycle implements BeforeAllCallback, AfterAllCallback, ParameterResolver {
   // This is a public map so that objects outside test scope can access the server.
   // For example, nested classes inside test classes that define custom options and need the server.
   public static Map<Class<?>, Server> serverMap;
 
   static {
     serverMap = new HashMap<>();
+  }
+
+  @Override
+  public void beforeAll(ExtensionContext extensionContext) throws Exception {
+    getOrCreateServer(extensionContext);
   }
 
   @Override
@@ -28,8 +32,25 @@ public class ServerLifecycle implements BeforeAllCallback, AfterAllCallback {
   }
 
   @Override
-  public void beforeAll(ExtensionContext extensionContext) throws Exception {
-    Server server = Server.createHttp(nextFreePort());
-    serverMap.put(extensionContext.getRequiredTestClass(), server);
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return Server.class.equals(parameterContext.getParameter().getType());
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return getOrCreateServer(extensionContext);
+  }
+
+  private Server getOrCreateServer(ExtensionContext extensionContext) {
+    Server server = serverMap.get(extensionContext.getRequiredTestClass());
+    if(server == null) {
+      try {
+        server = Server.createHttp(nextFreePort());
+        serverMap.put(extensionContext.getRequiredTestClass(), server);
+      } catch (IOException e) {
+        throw new ParameterResolutionException(e.getMessage());
+      }
+    }
+    return server;
   }
 }
