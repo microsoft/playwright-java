b9b3552cc41dc76985b4c07425f762c5d15c02d3
Yury Semikhatsky <yurys@chromium.org>
2024-02-20 10:58:32 -0800

    chore: roll driver, fix  canSpecifyPreinstalledNodeJsAsEnv on windows (#1496)

--- playwright/src/test/java/com/microsoft/playwright/TestPageAddScriptTag.java
+++ playwright/src/test/java/com/microsoft/playwright/TestPageAddScriptTag.java
@@ -3,6 +3,7 @@ package com.microsoft.playwright;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIf;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -16,6 +17,6 @@ public class TestPageAddScriptTag extends TestBase {
     Path path = Paths.get("src/test/resources/injectedfile.js");
     page.addScriptTag(new Page.AddScriptTagOptions().setPath(path));
     String result = (String) page.evaluate("() => window['__injectedError'].stack");
-    assertTrue(result.contains("resources/injectedfile.js"));
+    assertTrue(result.contains("resources" + File.separator + "injectedfile.js"), result);
   }
 }
