d37d039ca891ef192aaf8a6698bacecb8ab5e9b5
Yury Semikhatsky <yurys@chromium.org>
2024-06-24 16:18:53 -0700

    devops: roll_driver script

--- tools/test-cli-version/test.sh
+++ tools/test-cli-version/test.sh
@@ -15,14 +15,14 @@ mvn exec:java -e -Dexec.mainClass=com.microsoft.playwright.CLI -Dexec.args="--ve
 echo "Running TestApp..."
 mvn compile exec:java -e -Dexec.mainClass=com.microsoft.playwright.testcliversion.TestApp 2>&1 | tee ${TMP_DIR}/app.txt
 
-CLI_VERSION=$(cat ${TMP_DIR}/cli.txt | tail -n 1 | cut -d\  -f2)
+DRIVER_VERSION=$(cat ${TMP_DIR}/cli.txt | tail -n 1 | cut -d\  -f2)
 PACKAGE_VERSION=$(cat ${TMP_DIR}/app.txt | grep ImplementationVersion | cut -d\  -f2)
 
 rm -rf $TMP_DIR
 
-echo "Comparing versions: ${CLI_VERSION} and ${PACKAGE_VERSION}"
+echo "Comparing versions: ${DRIVER_VERSION} and ${PACKAGE_VERSION}"
 
-if [[ "$CLI_VERSION" == "$PACKAGE_VERSION" ]];
+if [[ "$DRIVER_VERSION" == "$PACKAGE_VERSION" ]];
 then
   echo "SUCCESS.";
 else
