02863c59e6df00c8a5d110d88b7021165e59ad5c
Yury Semikhatsky <yurys@chromium.org>
2024-11-14 14:31:16 -0800

    unshift

--- playwright/src/test/java/com/microsoft/playwright/TestPageAriaSnapshot.java
+++ playwright/src/test/java/com/microsoft/playwright/TestPageAriaSnapshot.java
@@ -4,14 +4,43 @@ import com.microsoft.playwright.junit.FixtureTest;
 import com.microsoft.playwright.junit.UsePlaywright;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @FixtureTest
 @UsePlaywright
 public class TestPageAriaSnapshot {
+  public static String unshift(String snapshot) {
+    List<String> lines = Arrays.asList(snapshot.split("\n"));
+    int whitespacePrefixLength = 100;
+    Pattern pattern = Pattern.compile("^(\\s*).*");
+    for (String line : lines) {
+      if (line.trim().isEmpty())
+        continue;
+      Matcher matcher = pattern.matcher(line);
+      if (!matcher.matches()) {
+        continue;
+      }
+      String match = matcher.group(1);
+      if (match.length() < whitespacePrefixLength) {
+        whitespacePrefixLength = match.length();
+      }
+    }
+    final int prefixLength = whitespacePrefixLength;
+    return lines.stream()
+      .filter(line -> !line.trim().isEmpty())
+      .map(line -> line.substring(prefixLength))
+      .collect(Collectors.joining("\n"));
+  }
+
   private static void checkAndMatchSnapshot(Locator locator, String snapshot) {
-    assertEquals(snapshot, locator.ariaSnapshot());
+    assertEquals(unshift(snapshot), locator.ariaSnapshot());
     assertThat(locator).matchesAriaSnapshot(snapshot);
   }
 
@@ -24,7 +53,9 @@ public class TestPageAriaSnapshot {
   @Test
   void shouldSnapshotList(Page page) {
     page.setContent("<h1>title</h1><h1>title 2</h1>");
-    checkAndMatchSnapshot(page.locator("body"), "- heading \"title\" [level=1]\n- heading \"title 2\" [level=1]");
+    checkAndMatchSnapshot(page.locator("body"), "" +
+      "  - heading \"title\" [level=1]\n" +
+      "  - heading \"title 2\" [level=1]");
   }
 
   @Test
@@ -42,7 +73,8 @@ public class TestPageAriaSnapshot {
   @Test
   void shouldAllowTextNodes(Page page) {
     page.setContent("<h1>Microsoft</h1><div>Open source projects and samples from Microsoft</div>");
-    checkAndMatchSnapshot(page.locator("body"), "- heading \"Microsoft\" [level=1]\n- text: Open source projects and samples from Microsoft");
+    checkAndMatchSnapshot(page.locator("body"), "- heading \"Microsoft\" [level=1]\n" +
+      "- text: Open source projects and samples from Microsoft");
   }
 
   @Test
