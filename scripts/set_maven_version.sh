f13bc6ec4a8502ad9fec78a3676b2110f86a2876
Max Schmitt <max@schmitt.mx>
2025-02-10 13:47:16 +0100

    chore: auto update example pw version in pom

--- scripts/set_maven_version.sh
+++ scripts/set_maven_version.sh
@@ -20,7 +20,12 @@ POM_FILES=(
   examples/pom.xml
 )
 
-for name in ${POM_FILES[*]};
-do
-  mvn versions:set -D generateBackupPoms=false -D newVersion=$VERSION -f $name
-done
+node -e "$(cat <<EOF
+  const parts = process.argv[1].split('.').map(part => parseInt(part, 10));
+  parts[1]--;
+  const previousMajorVersion = parts.join('.');
+  fs.writeFileSync('examples/pom.xml', fs.readFileSync('examples/pom.xml', 'utf8')
+    .replace(/<playwright-version>.*<\/playwright-version>/, '<playwright-version>' + previousMajorVersion + '</playwright-version>')
+  );
+EOF
+)" $VERSION
