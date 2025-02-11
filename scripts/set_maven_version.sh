464963839ffe1df583d0ec26a5c21889d5396603
Max Schmitt <max@schmitt.mx>
2025-02-11 12:37:49 +0100

    review feedback

--- scripts/set_maven_version.sh
+++ scripts/set_maven_version.sh
@@ -21,11 +21,11 @@ POM_FILES=(
 )
 
 node -e "$(cat <<EOF
-  const parts = process.argv[1].split('.').map(part => parseInt(part, 10));
+  const parts = process.argv[1].split('-')[0].split('.').map(part => parseInt(part, 10));
   parts[1]--;
   const previousMajorVersion = parts.join('.');
   fs.writeFileSync('examples/pom.xml', fs.readFileSync('examples/pom.xml', 'utf8')
-    .replace(/<playwright-version>.*<\/playwright-version>/, '<playwright-version>' + previousMajorVersion + '</playwright-version>')
+    .replace(/<playwright\.version>.*<\/playwright\.version>/, '<playwright\.version>' + previousMajorVersion + '</playwright\.version>')
   );
 EOF
 )" $VERSION
