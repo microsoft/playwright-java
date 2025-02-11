ec48e0e7cd8b44c0b3366a64385e99e661142865
Max Schmitt <max@schmitt.mx>
2025-02-11 23:28:12 +0100

    move to roll_driver

--- scripts/set_maven_version.sh
+++ scripts/set_maven_version.sh
@@ -19,13 +19,3 @@ POM_FILES=(
   tools/*/pom.xml
   examples/pom.xml
 )
-
-node -e "$(cat <<EOF
-  const parts = process.argv[1].split('-')[0].split('.').map(part => parseInt(part, 10));
-  parts[1]--;
-  const previousMajorVersion = parts.join('.');
-  fs.writeFileSync('examples/pom.xml', fs.readFileSync('examples/pom.xml', 'utf8')
-    .replace(/<playwright\.version>.*<\/playwright\.version>/, '<playwright\.version>' + previousMajorVersion + '</playwright\.version>')
-  );
-EOF
-)" $VERSION
