3c925b1796382592c344869ac6f3f66adfcd9bfe
ArcticFoox <jongsin16@naver.com>
2025-07-21 16:27:58 +0900

    test: add classloader docker ci test

--- tools/test-spring-classloader/src/main/java/com/microsoft/playwright/springboottest/TestApp.java
+++ tools/test-spring-classloader/src/main/java/com/microsoft/playwright/springboottest/TestApp.java
@@ -15,12 +15,12 @@ public class TestApp implements CommandLineRunner {
   }
 
   public void run(String... args) {
-    System.out.println("Starting original Playwright test...");
+    // use CompletableFuture to run Playwright asynchronously
     CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> {
       try (Playwright playwright = Playwright.create()) {
-        System.out.println("original Playwright test started, waiting for completion...");
+        System.out.println("Playwright classLoader test started, waiting for completion...");
         BrowserType browserType = getBrowserTypeFromEnv(playwright);
-        System.out.println("Running original test with " + browserType.name());
+        System.out.println("Running test with " + browserType.name());
         Browser browser = browserType.launch();
         BrowserContext context = browser.newContext();
         Page page = context.newPage();
@@ -33,40 +33,11 @@ public class TestApp implements CommandLineRunner {
       }
     });
 
-    System.out.println("original Playwright test is running asynchronously, main thread will wait for it to complete.");
+    System.out.println("Playwright classLoader test is running asynchronously, main thread will wait for it to complete.");
 
     voidCompletableFuture.join();
 
-    System.out.println("original Playwright test completed.");
-
-
-    System.out.println("Starting new Playwright test...");
-
-    // Set the new driver implementation to use the DriverJar class
-    System.setProperty( "playwright.driver.impl", "com.microsoft.playwright.springboottest.DriverJar" );
-
-    CompletableFuture<Void> voidCompletableFuture2 = CompletableFuture.runAsync(() -> {
-      try (Playwright playwright = Playwright.create()) {
-        System.out.println("new Playwright test started, waiting for completion...");
-        BrowserType browserType = getBrowserTypeFromEnv(playwright);
-        System.out.println("Running new test with " + browserType.name());
-        Browser browser = browserType.launch();
-        BrowserContext context = browser.newContext();
-        Page page = context.newPage();
-        System.out.println(page.evaluate("'SUCCESS: did evaluate in page'"));
-      } catch (Exception e) {
-        System.out.println("FAILED: " + e.toString());
-        for (StackTraceElement ste : e.getStackTrace()) {
-          System.out.println("\tat " + ste);
-        }
-      }
-    });
-
-    System.out.println("new Playwright test is running asynchronously, main thread will wait for it to complete.");
-
-    voidCompletableFuture2.join();
-
-    System.out.println("new Playwright test completed.");
+    System.out.println("Playwright classLoader test completed.");
 
   }
 
