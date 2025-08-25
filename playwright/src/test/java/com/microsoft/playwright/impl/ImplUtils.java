d677c1b6963d7da3cef74edd623bad553308e59f
Max Schmitt <max@schmitt.mx>
2025-08-26 00:09:18 +0200

    refactor: remove unused ImplUtils class

--- playwright/src/test/java/com/microsoft/playwright/impl/ImplUtils.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.playwright.impl;
-
-import com.microsoft.playwright.Browser;
-
-public class ImplUtils {
-  public static boolean isRemoteBrowser(Browser browser) {
-    return ((BrowserImpl) browser).isConnectedOverWebSocket;
-  }
-}
